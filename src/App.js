dd153d05e4962dda7d73effa7318ba30d81ca20f
Your Name <you@example.com>
2021-04-03 23:17:21 +0300

    arrange data into state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
-        
+        <h1>hello world</h1>
     </div>
   );
 }
---
+++ src/utilitis/Axios.js
@@ -0,0 +1,15 @@
+import Axios from 'axios'
+
+const API_KEY = "564ff4ab275baff4372adb3dc85ab368"
+
+const url = `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`
+const url = `https://api.themoviedb.org/3/movie?api_key=${API_KEY}&id_genres=28`
+const url = `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`
+const url = `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`
+
+// https://api.themoviedb.org/3/genre/movie/list?api_key=564ff4ab275baff4372adb3dc85ab368&language=en-US // get all ganers
+
+
+// https://api.themoviedb.org/<GENRE_ID>/genre/movie/list?api_key=<API_KEY>&language=en-US
+
+
