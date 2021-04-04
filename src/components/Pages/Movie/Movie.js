3008f27311c3c357cf8b829e8531428204ea7e7d
Your Name <you@example.com>
2021-04-04 15:34:27 +0300

    add trailer

--- src/components/Pages/Movie/Movie.js
+++ src/components/Pages/Movie/Movie.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
 import '../../../App.css'
+import Youtube from 'react-youtube'
 
 export default function Movie({match}) {
     const [id,SetId] = useState(match.params.id)
     const [Data,SetData] = useState([])
     const [url,SetUrl] = useState('')
+    const [movies,SetMovies] = useState('')
 
     const  ImgStartUrl = 'https://image.tmdb.org/t/p/w500/'
     useEffect(() =>{
@@ -14,14 +16,24 @@ export default function Movie({match}) {
             let Data = await Axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=564ff4ab275baff4372adb3dc85ab368&language=en-US`)
             SetData(Data.data)
             let Url = await Axios.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=564ff4ab275baff4372adb3dc85ab368&language=en-US`)
-            console.log(Url)
+            SetUrl(Url.data)
         
         }
         Request()
     },[])
     
     
+    const HandleTrailer = () =>{
+        if(url){
+            SetUrl('')
+        }else{
+    
+        }
+    }
+
+
     // console.log(Data)
+    console.log(url)
     return (
         <div className="movie-container">
             <div className="poster">
@@ -29,6 +41,7 @@ export default function Movie({match}) {
             </div>
             <div className="movie-data">
                 <h1>{Data.title}</h1>
+              {url && <Youtube videoId={url.results[0].key}></Youtube> }
                 <h3>{Data.overview}</h3>
 
                 <h5><a href={Data.homepage}>HomePage</a></h5>
