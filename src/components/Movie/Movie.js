41820d9a9c299ee24492cdf37df0a2177754d24f
Your Name <you@example.com>
2021-04-04 03:32:08 +0300

    add rating to cards

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 
 export default function Movie({imageUrl,title,rating}) {
+    
+    const  ImgStartUrl = 'https://image.tmdb.org/t/p/w500/'
+    
+    const [showRating,SetShowRating] = useState('')
+     
+    useEffect(() =>{
+
+    let ratingcolor = ''   
+
+    if(rating < 5){
+        ratingcolor  = "red"
+    }
+    else if(rating >5 && rating < 8){
+        ratingcolor = "orange"
+    }else{
+        ratingcolor = "green"
+    }
+    SetShowRating(ratingcolor)
+
+    },[])
+
+     
+
     return (
    
         <div className="movieCard">
-            <img src={`${imageUrl}`} alt="movie"/>
+            <img src={`${ImgStartUrl+imageUrl}`} alt="movie"/>
              
           <div className ="flex">
              <h3>{title}</h3>
-             <span className="movie-rating red">{rating}</span>
+             <span className={"movie-rating " + showRating}>{rating}</span>
            </div>
         </div>
    
